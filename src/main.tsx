b9ddf4e8f08812f92d7ae24dc6a5dd6eb6b26037
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-06-02 01:26:46 +0300

    final update

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 
+import "modern-normalize";
+
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
