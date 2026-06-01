9a1c3ab08a5f2701fa98d5d14f7f849d5d5fc075
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-06-01 15:24:34 +0300

    add component VoteOptions and update App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,11 +3,13 @@
 import css from './App.module.css';
 
 import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
 
 export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions />
     </div>
   );
 }
