5e41bc5bd9b7baa7f3bb14fa6826425fdbb42123
Anastasiia <anastasiia0matsiuk@gmail.com>
2026-06-02 01:19:16 +0300

    add component Notifiction and update App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import css from './App.module.css';
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
 import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
 
 import type { Votes, VoteType } from '../../types/votes';
 
@@ -40,14 +41,20 @@ export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
+
       <VoteOptions
         onVote={handleVote}
         onReset={resetVotes}
         canReset={canReset} />
-      <VoteStats
-        votes={votes}
-        totalVotes={totalVotes}
-        positiveRate={positiveRate} />
+      
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate} />
+      ) : (
+          <Notification />
+        )}
     </div>
   );
 }
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,10 @@
+// src/components/Notification/Notification.tsx
+
+import css from './Notification.module.css';
+
+
+export default function Notification() {
+    return (
+        <p className={css.message}>No feedback yet</p>
+    )
+}
